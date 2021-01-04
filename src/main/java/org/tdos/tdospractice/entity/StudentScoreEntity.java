a4d2c2843fb910b936c721aff3ac6d24e3dc7c9a
yy-majiangjiang <mjj@defimex.io>
2021-01-04 11:06:44 +0800

    add .gitignore

---
+++ src/main/java/org/tdos/tdospractice/entity/StudentScoreEntity.java
@@ -0,0 +1,29 @@
+package org.tdos.tdospractice.entity;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class StudentScoreEntity implements Serializable {
+
+    private String id;
+
+    private String sectionId;
+
+    private String userId;
+
+    private Integer score;
+
+    private Integer status;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+}
