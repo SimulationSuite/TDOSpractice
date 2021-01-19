46f5bb16ebbf61808ffae59e314b3b52b2d2ebcd
yy-majiangjiang <mjj@defimex.io>
2021-01-19 13:44:41 +0800

    add questionbackassignment api

---
+++ src/main/java/org/tdos/tdospractice/body/QuestionBackAssignment.java
@@ -0,0 +1,13 @@
+package org.tdos.tdospractice.body;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class QuestionBackAssignment {
+
+    @JsonProperty("assignment_id")
+    public String assignmentId;
+
+    @JsonProperty("question_id")
+    public String questionId;
+
+}
--- src/main/java/org/tdos/tdospractice/service/QuestionBackService.java
+++ src/main/java/org/tdos/tdospractice/service/QuestionBackService.java
@@ -2,7 +2,9 @@ package org.tdos.tdospractice.service;
 
 import javafx.util.Pair;
 import org.tdos.tdospractice.body.QuestionBack;
+import org.tdos.tdospractice.body.QuestionBackAssignment;
 import org.tdos.tdospractice.entity.QuestionBackEntity;
+import org.tdos.tdospractice.entity.QuestionBackAssignmentEntity;
 
 import java.util.List;
 import java.util.Map;
@@ -16,5 +18,7 @@ public interface QuestionBackService {
     Boolean modifyQuestionBackById(QuestionBack questionBack);
 
     QuestionBackEntity getStudentAnswerByAssignment(String assignmentId);
+
+    QuestionBackAssignmentEntity addQuestionBackAssignment(QuestionBackAssignment questionBackAssignment);
 
 }
