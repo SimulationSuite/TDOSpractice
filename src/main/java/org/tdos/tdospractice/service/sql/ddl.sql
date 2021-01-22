b5124b785f7f1418482654c528e3459657e51798
liuyong199311080810 <1991595613@qq.com>
2021-01-22 11:42:04 +0800

    modify qql.sql

--- src/main/java/org/tdos/tdospractice/service/sql/ddl.sql
+++ src/main/java/org/tdos/tdospractice/service/sql/ddl.sql
@@ -128,6 +128,8 @@ create table if not exists courseware(
     kind int4 default 0,
     "type" int4 default 0,
     "url" varchar(255) DEFAULT NULL,
+    duration int8 DEFAULT NULL,
+    "size" int8 DEFAULT 0,
     category_id varchar(255),
     created_at TIMESTAMP(0)  without time zone default (now() at time zone 'utc'),
     updated_at TIMESTAMP(0)  without time zone default (now() at time zone 'utc')
