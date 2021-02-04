62568350bc4b0b3d86aa2a5e6ddd9a6ac9e9dbee
YunMin <347594248@qq.com>
2021-02-04 13:45:48 +0800

    update KvmManager.java

--- src/main/java/org/tdos/tdospractice/kvm/KvmManager.java
+++ src/main/java/org/tdos/tdospractice/kvm/KvmManager.java
@@ -83,7 +83,7 @@ public class KvmManager {
 
     private String getURL(ImageEntity imageEntity, int port, String IP) {
         if (imageEntity.getKind() == DockerTool.Type.GUI.ordinal()) {
-            return String.format("http://%s:%i%s", IP, port, imageEntity.getUrl());
+            return String.format("ws://%s:%d%s", IP, port, imageEntity.getUrl());
         }
         return null;
     }
