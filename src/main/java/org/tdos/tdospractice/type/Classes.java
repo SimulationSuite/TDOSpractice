7e5c8fdcdbc1ea4d8e414d6547f9613aa24375ad
liuyong199311080810 <1991595613@qq.com>
2021-02-18 15:28:50 +0800

    1

--- src/main/java/org/tdos/tdospractice/type/Classes.java
+++ src/main/java/org/tdos/tdospractice/type/Classes.java
@@ -1,11 +1,15 @@
 package org.tdos.tdospractice.type;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public class Classes {
 
+    @JsonProperty("class_id")
     public String classId;
 
+    @JsonProperty("user_id_list")
     public List<String> userIds;
 
     public Boolean completed;
