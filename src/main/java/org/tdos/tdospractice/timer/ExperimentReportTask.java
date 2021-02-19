5f225b151e09dfe6705fd184b97cbcbd4fe193ca
Zt <840043122@qq.com>
2021-02-19 14:22:28 +0800

    add timer

---
+++ src/main/java/org/tdos/tdospractice/timer/ExperimentReportTask.java
@@ -0,0 +1,23 @@
+package org.tdos.tdospractice.timer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.tdos.tdospractice.service.ExperimentReportService;
+import org.tdos.tdospractice.service.ExperimentService;
+
+import java.time.LocalDateTime;
+
+@Configuration
+@EnableScheduling
+public class ExperimentReportTask {
+
+    @Autowired
+    private ExperimentReportService experimentReportService;
+
+//    @Scheduled(cron = "0 0 0 * * ?")
+//    private void configureTasks() {
+//        experimentReportService.updateExperimentReportByEndtime();
+//    }
+}
