3643ef774a898a990ef91d7ac2b29d3a64b3991d
liuyong199311080810 <1991595613@qq.com>
2021-02-24 16:01:07 +0800

    1

--- src/main/java/org/tdos/tdospractice/service/Impl/CourseServiceImpl.java
+++ src/main/java/org/tdos/tdospractice/service/Impl/CourseServiceImpl.java
@@ -95,10 +95,10 @@ public class CourseServiceImpl implements CourseService {
     @Override
     public Pair<Boolean, Object> getAdminCourseListByClassId(String classId, Integer perPage, Integer page) {
         if (ObjectUtils.isEmpty(classId)) {
-            return new Pair<>(false, "class_id is not be null");
+            return new Pair<>(false, "班级id是空");
         }
         if (!UUIDPattern.isValidUUID(classId)) {
-            return new Pair<>(false, "class_id is not be uuid");
+            return new Pair<>(false, "班级id不是uuid");
         }
         PageHelper.startPage(page, perPage);
         PageHelper.orderBy("c.created_at desc");
@@ -121,28 +121,28 @@ public class CourseServiceImpl implements CourseService {
     public Pair<Boolean, PrepareCourseReturn> prepareCourse(PrepareCourse prepareCourse) {
         PrepareCourseReturn prepareCourseReturn = new PrepareCourseReturn();
         if (ObjectUtils.isEmpty(prepareCourse.courseId)) {
-            prepareCourseReturn.setErrMessage("course_id can not be null");
+            prepareCourseReturn.setErrMessage("课程id不为空");
             return new Pair<>(false, prepareCourseReturn);
         }
         if (ObjectUtils.isEmpty(prepareCourse.userId)) {
-            prepareCourseReturn.setErrMessage("user_id can not be null");
+            prepareCourseReturn.setErrMessage("userId不能为空");
             return new Pair<>(false, prepareCourseReturn);
         }
         if (!UUIDPattern.isValidUUID(prepareCourse.courseId)) {
-            prepareCourseReturn.setErrMessage("course_id is not be uuid");
+            prepareCourseReturn.setErrMessage("课程id不是uuid");
             return new Pair<>(false, prepareCourseReturn);
         }
         if (courseMapper.hasCourseExist(prepareCourse.courseId) == 0) {
-            prepareCourseReturn.setErrMessage("course is not exist");
+            prepareCourseReturn.setErrMessage("课程不存在");
             return new Pair<>(false, prepareCourseReturn);
         }
         Course course = courseMapper.getCourseByCourseId(prepareCourse.courseId);
         if (course == null || ObjectUtils.isEmpty(course.id)) {
-            prepareCourseReturn.setErrMessage("select course is not exist");
+            prepareCourseReturn.setErrMessage("课程不存在");
             return new Pair<>(false, prepareCourseReturn);
         }
         if (course.type == 1) {
-            prepareCourseReturn.setErrMessage("select course is not admin public");
+            prepareCourseReturn.setErrMessage("课程不是管理员发布的");
             return new Pair<>(false, prepareCourseReturn);
         }
         String modelId = courseMapper.getModelCourse(prepareCourse.courseId, prepareCourse.userId);
@@ -240,7 +240,7 @@ public class CourseServiceImpl implements CourseService {
         if (courses.size() > 0) {
             List<Course> coursesList = courseMapper.getCourseListByIdPerfect(pageInfo.getList().stream().map(x -> x.id).collect(Collectors.toList()));
             coursesList.forEach(course -> {
-                if (course.endAt != null && course.endAt.isBefore(LocalDateTime.now())) {
+                if (course.status != 0 && course.endAt != null && course.endAt.isBefore(LocalDateTime.now())) {
                     course.status = -1;
                 }
             });
@@ -263,22 +263,22 @@ public class CourseServiceImpl implements CourseService {
     @Transactional(rollbackFor = {RuntimeException.class, Error.class})
     public Pair<Boolean, Object> AddAdminCourse(AddCourse addCourse) {
         if (ObjectUtils.isEmpty(addCourse.name)) {
-            return new Pair<>(false, "name can not be null");
+            return new Pair<>(false, "章名不存在");
         }
         if (courseMapper.hasCourseNameExist(addCourse.name) > 0) {
-            return new Pair<>(false, "course name has been existed");
+            return new Pair<>(false, "课程不存在");
         }
         if (ObjectUtils.isEmpty(addCourse.picUrl)) {
-            return new Pair<>(false, "pic_url  can not be null");
+            return new Pair<>(false, "图片链接不存在");
         }
         if (ObjectUtils.isEmpty(addCourse.introduction)) {
-            return new Pair<>(false, "introduction  can not be null");
+            return new Pair<>(false, "课程简介不能为空");
         }
         if (ObjectUtils.isEmpty(addCourse.ownerId)) {
-            return new Pair<>(false, "owner_id can not be null");
+            return new Pair<>(false, "课程ownedId不存在");
         }
         if (addCourse.chapters == null) {
-            return new Pair<>(false, "chapters can not be null");
+            return new Pair<>(false, "章不能为空");
         }
         Course course = new Course();
         course.name = addCourse.name;
@@ -311,29 +311,29 @@ public class CourseServiceImpl implements CourseService {
     @Transactional(rollbackFor = {RuntimeException.class, Error.class})
     public Pair<Boolean, String> modifyCourseStatus(ModifyCourseStatus modifyCourseStatus) {
         if (ObjectUtils.isEmpty(modifyCourseStatus.ownerId)) {
-            return new Pair<>(false, "owner_id can not be null");
+            return new Pair<>(false, "拥有者不能是空");
         }
         if (ObjectUtils.isEmpty(modifyCourseStatus.courseId)) {
-            return new Pair<>(false, "course_id  can not be null");
+            return new Pair<>(false, "课程id不能是空");
         }
         if (!UUIDPattern.isValidUUID(modifyCourseStatus.courseId)) {
-            return new Pair<>(false, "course_id is not be uuid");
+            return new Pair<>(false, "课程id不是uuid");
         }
         if (courseMapper.hasCourseExist(modifyCourseStatus.courseId) == 0) {
-            return new Pair<>(false, "course is not exist");
+            return new Pair<>(false, "课程id不存在");
         }
         Course course = courseMapper.getCourseByCourseId(modifyCourseStatus.courseId);
         if (!course.ownerId.equals(modifyCourseStatus.ownerId)) {
-            return new Pair<>(false, "course is not belong to owner_id: " + modifyCourseStatus.ownerId);
+            return new Pair<>(false, "课程不属于拥有者: " + modifyCourseStatus.ownerId);
         }
         if (modifyCourseStatus.status != null && courseMapper.hasTeacherCourseNameExist(course.name, modifyCourseStatus.ownerId) > 0) {
-            return new Pair<>(false, "course name has been existed");
+            return new Pair<>(false, "课程名已存在");
         }
         courseMapper.modifyCourseStatus(modifyCourseStatus.courseId, modifyCourseStatus.status == null || modifyCourseStatus.status == 0 ? 0 : 1, modifyCourseStatus.start, modifyCourseStatus.end);
         if (modifyCourseStatus.userIds != null && modifyCourseStatus.userIds.size() > 0) {
             if (!modifyCourseStatus.userIds.stream().allMatch((userId -> userMapper.findUserById(userId) != null
                     && userMapper.findUserById(userId).getRoleID() == 2))) {
-                return new Pair<>(false, "user_id list is not exist or not all student");
+                return new Pair<>(false, "学生列表不存在或者不全是学生");
             }
             classCourseMapper.deleteByCourseId(modifyCourseStatus.courseId);
             modifyCourseStatus.userIds.forEach(userId -> {
@@ -482,10 +482,10 @@ public class CourseServiceImpl implements CourseService {
     @Override
     public Pair<Boolean, Object> getCourseById(String courseId) {
         if (ObjectUtils.isEmpty(courseId)) {
-            return new Pair<>(false, "course_id is not be null");
+            return new Pair<>(false, "课程id不能是空");
         }
         if (!UUIDPattern.isValidUUID(courseId)) {
-            return new Pair<>(false, "course_id is not be uuid");
+            return new Pair<>(false, "课程id不是uuid");
         }
         Course course = courseMapper.getCourseById(courseId);
         if (course != null) {
@@ -560,10 +560,10 @@ public class CourseServiceImpl implements CourseService {
     @Transactional(rollbackFor = {RuntimeException.class, Error.class})
     public Pair<Boolean, Object> AddAdminCourseCompleted(AddCourseCompleted addCourseCompleted) {
         if (ObjectUtils.isEmpty(addCourseCompleted.courseId)) {
-            return new Pair<>(false, "course_id is not be null");
+            return new Pair<>(false, "课程id不能是空");
         }
         if (ObjectUtils.isEmpty(addCourseCompleted.chapters) || addCourseCompleted.chapters.size() == 0) {
-            return new Pair<>(false, "chapters is not be null");
+            return new Pair<>(false, "章不能是空");
         }
         Course course = courseMapper.getCourseById(addCourseCompleted.courseId);
         course.chapters.forEach(chapter -> chapterMapper.removeChapter(chapter.id));
@@ -615,13 +615,13 @@ public class CourseServiceImpl implements CourseService {
     @Transactional(rollbackFor = {RuntimeException.class, Error.class})
     public Pair<Boolean, String> insertCourseChapterCompleted(AddChapterCompleted addChapterCompleted) {
         if (ObjectUtils.isEmpty(addChapterCompleted.courseId)) {
-            return new Pair<>(false, "course_id is not be null");
+            return new Pair<>(false, "课程id不能是空");
         }
         if (ObjectUtils.isEmpty(addChapterCompleted.chapter)) {
-            return new Pair<>(false, "chapter is not be null");
+            return new Pair<>(false, "章不能是空");
         }
         if (!UUIDPattern.isValidUUID(addChapterCompleted.courseId)) {
-            return new Pair<>(false, "course_id is not be uuid");
+            return new Pair<>(false, "课程id不是uuid");
         }
         Course course = courseMapper.getCourseById(addChapterCompleted.courseId);
         if (course.chapters.stream().map(Chapter::getId)
@@ -679,7 +679,7 @@ public class CourseServiceImpl implements CourseService {
     @Transactional(rollbackFor = {RuntimeException.class, Error.class})
     public Pair<Boolean, String> modifyExpiredCourseStatus(ModifyExpiredCourseStatus modifyExpiredCourseStatus) {
         if (ObjectUtils.isEmpty(modifyExpiredCourseStatus.ownerId)) {
-            return new Pair<>(false, "owner_id can not be null");
+            return new Pair<>(false, "拥有者不能是空");
         }
         courseMapper.modifyExpiredCourseStatus(modifyExpiredCourseStatus.ownerId);
         return new Pair<>(true, "");
@@ -711,17 +711,17 @@ public class CourseServiceImpl implements CourseService {
     @Transactional(rollbackFor = {RuntimeException.class, Error.class})
     public Pair<Boolean, String> removeCourseById(DeleteCourse deleteCourse) {
         if (ObjectUtils.isEmpty(deleteCourse.ownerId)) {
-            return new Pair<>(false, "owner_id can not be null");
+            return new Pair<>(false, "拥有者不能是空");
         }
         if (ObjectUtils.isEmpty(deleteCourse.courseId)) {
-            return new Pair<>(false, "course_id  can not be null");
+            return new Pair<>(false, "课程id不能是空");
         }
         if (!UUIDPattern.isValidUUID(deleteCourse.courseId)) {
-            return new Pair<>(false, "course_id is not be uuid");
+            return new Pair<>(false, "课程id不是uuid");
         }
         Course course = courseMapper.getCourseByCourseId(deleteCourse.courseId);
         if (!course.ownerId.equals(deleteCourse.ownerId)) {
-            return new Pair<>(false, "course is not belong to owner_id: " + deleteCourse.ownerId);
+            return new Pair<>(false, "课程不属于这个拥有者: " + deleteCourse.ownerId);
         }
         course.chapters.stream().filter(chapter -> !chapter.id.equals(EMPTY_UUID)).forEach(chapter -> {
             chapter.sections.stream().filter(section -> !section.id.equals(EMPTY_UUID)).forEach(section -> {
@@ -739,11 +739,11 @@ public class CourseServiceImpl implements CourseService {
     @Override
     public Pair<Boolean, String> modifyCourseName(ModifyCourseName modifyCourseName) {
         if (courseMapper.hasCourseExist(modifyCourseName.courseId) == 0) {
-            return new Pair<>(false, "course is not exist");
+            return new Pair<>(false, "课程不存在");
         }
         Course course = courseMapper.getCourseByCourseId(modifyCourseName.courseId);
         if (!course.ownerId.equals(modifyCourseName.ownerId)) {
-            return new Pair<>(false, "course is not belong to owner_id: " + modifyCourseName.ownerId);
+            return new Pair<>(false, "课程不属于这个拥有者: " + modifyCourseName.ownerId);
         }
         courseMapper.modifyCourseName(modifyCourseName.courseId, modifyCourseName.courseName);
         return new Pair<>(true, "");
